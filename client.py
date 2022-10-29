5f490e1fe1d36bc88666ebe50dcd4373c40d87a4
ebogdanov <ebogdanov@msk.rsvo.ru>
2022-10-29 23:35:55 +0300

    Готово.

--- client.py
+++ client.py
@@ -2,18 +2,24 @@ import asyncio
 import aiohttp
 
 async  def main():
-    # async with aiohttp.ClientSession() as session:
-    #     async with session.get('http://127.0.0.1:8080/user/80') as response:
-    #         print (await response.json())
 
     async with aiohttp.ClientSession() as session:
-        async with session.post(
-                'http://127.0.0.1:8080/user/',
-                json={'username': 'user_3',
-                      'password': '1234'},
-
-        )as response:
+        async with session.delete('http://127.0.0.1:8080/ad/1') as response:
             print(await response.text())
+
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.get('http://127.0.0.1:8080/ad/2') as response:
+    #         print (await response.json())
+
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.post(
+    #             'http://127.0.0.1:8080/ad/',
+    #             json={'id': 2,
+    #                   'title': 'Продам гараж 2',
+    #                   'description': 'Кирпичный, с ямой и гнилым полом',
+    #                   'owner': 'Вася'},
+    #     )as response:
+    #         print(await response.text())
 
 
 asyncio.run(main())
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import json
 import bcrypt
+from datetime import datetime
 from aiohttp import web
 from gino import Gino
 from asyncpg.exceptions import UniqueViolationError
@@ -24,69 +25,69 @@ class NotFound(HTTPExeption):
     status_code = 404
 
 
-class UserModel(db.Model):
-    __tablename__ = 'users'
-
+class AdModel(db.Model):
+    __tablename__ = 'advertisements'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False, unique=True)
+    description = db.Column(db.String, nullable=False)
+    registration_time = db.Column(db.DateTime, server_default='now()')
+    owner = db.Column(db.String, nullable=False)
+    _idx1 =db.Index('app_advertisements_title', 'title', unique=True)
 
-    _idx1 =db.Index('app_users_username', 'username', unique=True)
 
-
-class UserView(web.View):
+class AdView(web.View):
     async def get(self):
-        user_id = int(self.request.match_info['user_id'])
-        user = await UserModel.get(user_id)
-        if user is None:
-            raise NotFound(error= 'user does not exist')
+        adv_id = int(self.request.match_info['ad_id'])
+        adv = await AdModel.get(adv_id)
+        if adv is None:
+            raise NotFound(error='No adv with such title')
         return web.json_response({
-            'id': user.id,
-            'username': user.username
+            'id': adv.id,
+            'title': adv.title,
+            'description': adv.description,
+            'registration_time': str(adv.registration_time),
+            'owner': adv.owner
         })
 
     async def post(self):
         user_data = await self.request.json()
-        user_data['password'] = bcrypt.hashpw(
-            user_data['password'].encode(),
-            bcrypt.gensalt()
-                ).decode()
         try:
-            new_user = await UserModel.create(**user_data)
-            # new_user = await UserModel.create(username=user_data['username'],
-            #                                   password=user_data['password'])
+            new_ad = await AdModel.create(**user_data)
         except UniqueViolationError:
-            raise BadRequest(error='user already exist.')
-
-            # raise web.HTTPBadRequest(text=json.dumps({'error': 'user already exist.'}),
-            #                          content_type='application/json')
-            # return web.json_response(
-            #     {'error': 'user already exist.'}
-            # )
+            raise BadRequest(error='AD already exist.')
 
         return web.json_response(
-            {'user_id': new_user.id,
-             'ph': new_user.password}
+            {'id': new_ad.id,
+             'title': new_ad.title,
+             'description': new_ad.description,
+             'registration_time': str(new_ad.registration_time),
+             'owner': new_ad.owner
+             }
         )
+
+    async def delete(self):
+        adv_id = int(self.request.match_info['ad_id'])
+        adv = await AdModel.get(adv_id)
+        if adv is None:
+            raise NotFound(error='No adv with such title')
+        await adv.delete()
+        return web.json_response({
+            'id': adv_id,
+            'status': 'deleted'
+        })
 
 
 async def init_orm(app):
-    print('Придложение стартовало')
     await db.set_bind(PG_DSN)
     await db.gino.create_all()
     yield    #Всё, что до yield произойдет при запуске app, что после - при её завершении.
     await db.pop_bind().close()
-    print('Придложение закрывается')
 
-async def test(request):
 
-    return web.json_response(
-        {'hello': 'world'}
-    )
+app.router.add_route('GET', '/ad/{ad_id:\d+}', AdView)
+app.router.add_route('POST', '/ad/', AdView)
+app.router.add_route('DELETE', '/ad/{ad_id:\d+}', AdView)
 
-app.router.add_route('POST', '/test/', test)
-app.router.add_route('GET', '/user/{user_id:\d+}', UserView)
-app.router.add_route('POST', '/user/', UserView)
 
 app.cleanup_ctx.append(init_orm)
 
